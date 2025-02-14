6309f4a4aa104db3b71282af8c4b8636f38c2393
WineBlackdog <Iopuj2478@masseyhigh.school.nz>
2025-02-14 13:11:54 +1300

    Flexible multi-line statement generator component

---
+++ Sandpit.py
@@ -0,0 +1,14 @@
+# reminder - here is and integer that has been multiplied
+number = 5
+print(number * 3)
+
+# here ois a string that has been multipled by 3
+# (note the speech marks and text colour)
+num_string = "5"
+print(num_string * 3)
+
+# an example showing what the len(foo) method does
+example_text = "hello world"
+text_length = len(example_text)
+
+print(f"'{example_text}' is {text_length} character long ")
